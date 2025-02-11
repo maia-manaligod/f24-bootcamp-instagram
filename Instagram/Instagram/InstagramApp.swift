600428f54bde10fa78910d6434bc1defc13d722b
maia <maiam.unc@gmail.com>
2025-02-11 15:43:10 -0500

    checkpoint

---
+++ Instagram/Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by Maia Manaligod on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Instagram/Instagram/PostView.swift
@@ -0,0 +1,20 @@
+//
+//  PostView.swift
+//  Instagram
+//
+//  Created by Maia Manaligod on 9/17/24.
+//
+
+import SwiftUI
+
+struct PostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PostView_Previews: PreviewProvider {
+    static var previews: some View {
+        PostView()
+    }
+}
